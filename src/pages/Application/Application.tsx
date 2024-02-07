99bab75da9689b053c0936bf31a2a5ab6c2a30d8
AM1G0S <suleymandzyazikov@gmail.com>
2024-02-08 01:17:01 +0300

    добавил ÐнебеоÐошльшой фикс

--- src/pages/Application/Application.tsx
+++ src/pages/Application/Application.tsx
@@ -1,9 +1,7 @@
-import {FC, useState} from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {AppDispatch} from "../../redux/store.ts";
+import {FC} from "react";
 
 export const Application: FC = () => {
-	const [isLoading, setIsLoading] = useState<boolean>(false);
+	// const [isLoading, setIsLoading] = useState<boolean>(false);
 	
 	return (
 		<>
