ff3930a551051220743d7bb641b2a73a2d25db41
AM1G0S <suleymandzyazikov@gmail.com>
2024-02-12 14:30:52 +0300

    дreviews update

--- src/components/ui/ReviewsSlider/ReviewsSlider.tsx
+++ src/components/ui/ReviewsSlider/ReviewsSlider.tsx
@@ -2,6 +2,7 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Review } from '../Review/Review';
+declare module 'react-slick';
 
 import cls from './ReviewsSlider.module.scss'
 
