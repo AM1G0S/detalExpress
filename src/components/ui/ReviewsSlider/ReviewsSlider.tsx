93c335530057f4e27d8f0d4cf25f9ae1c296a2a0
AM1G0S <suleymandzyazikov@gmail.com>
2024-02-12 13:19:55 +0300

    добавил блок с отÑзывами

---
+++ src/components/ui/ReviewsSlider/ReviewsSlider.tsx
@@ -0,0 +1,108 @@
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Review } from '../Review/Review';
+
+import cls from './ReviewsSlider.module.scss'
+
+const reviews = [
+	{
+		id: 1,
+		name: "Валерия",
+		lastName: "Иванова",
+		avatar: "https://i.pravatar.cc/300?u=a042581f4e29026704d",
+		text: "Очень понравилось. Спасибо за качественную работу. Может еще раз попробовать?",
+		city: 'Москва',
+		platform: 'yandex'
+	},
+	{
+		id: 2,
+		name: "Алексей",
+		lastName: "Петров",
+		avatar: "https://i.pravatar.cc/300?u=a042581f4e29026704d",
+		text: "Очень понравилось. Спасибо за качественную работу. Может еще раз попробовать?",
+		city: 'Москва',
+		platform: '2gis'
+	},
+	{
+		id: 3,
+		name: "Иван",
+		lastName: "Сидоров",
+		avatar: "https://i.pravatar.cc/300?u=a042581f4e29026704d",
+		text: "Очень понравилось. Спасибо за качественную работу. Может еще раз попробовать?",
+		city: 'Санкт-Петербург',
+		platform: 'yandex'
+	},
+	{
+		id: 4,
+		name: "Екатерина",
+		lastName: "Кексова",
+		avatar: "https://i.pravatar.cc/300?u=a042581f4e29026704d",
+		text: "Очень понравилось. Спасибо за качественную работу. Может еще раз попробовать?",
+		city: 'Морки',
+		platform: '2gis'
+	},
+	{
+		id: 5,
+		name: "Ахреп",
+		lastName: "Луков",
+		avatar: "https://i.pravatar.cc/300?u=a042581f4e29026704d",
+		text: "Очень понравилось. Спасибо за качественную работу. Может еще раз попробовать?",
+		city: 'Самара',
+		platform: 'yandex'
+	},
+]
+
+export const ReviewsSlider = () => {
+	
+	const settings = {
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		infinite: true,
+		centerMode: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		initialSlide: 0,
+		speed: 500,
+		arrows: false,
+		adaptiveHeight: true,
+		appendDots: dots => <ul>{dots}</ul>,
+		customPaging: i => (
+			<div className="ft-slick__dots--custom">
+				<div className="loading" />
+			</div>
+		),
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 660,
+				settings: {
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					centerMode: false,
+					slidesToShow: 1,
+				}
+			}
+		]
+	};
+	
+	return (
+		<Slider className={cls.sliderWrapper} {...settings}>
+			{reviews.map((review) => (
+				<div key={review.id}>
+					<Review {...review} />
+				</div>
+			))}
+		</Slider>
+	);
+};
--- src/sections/Reviews/Reviews.tsx
+++ src/sections/Reviews/Reviews.tsx
@@ -1,67 +1,15 @@
 import classnames from "classnames";
 import {memo} from "react";
-import {Review} from "../../components";
+import {ReviewsSlider} from "../../components/";
 import styles from "../../styles/style.module.scss";
-import cls from "../Processing/Processing.module.scss";
-
-const reviews = [
-	{
-		id: 1,
-		name: "Валерия",
-		lastName: "Иванова",
-		avatar: "https://i.pravatar.cc/300?u=a042581f4e29026704d",
-		text: "Очень понравилось. Спасибо за качественную работу. Может еще раз попробовать?",
-		city: 'Москва',
-		platform: 'yandex'
-	},
-	{
-		id: 2,
-		name: "Алексей",
-		lastName: "Петров",
-		avatar: "https://i.pravatar.cc/300?u=a042581f4e29026704d",
-		text: "Очень понравилось. Спасибо за качественную работу. Может еще раз попробовать?",
-		city: 'Москва',
-		platform: '2gis'
-	},
-	{
-		id: 3,
-		name: "Иван",
-		lastName: "Сидоров",
-		avatar: "https://i.pravatar.cc/300?u=a042581f4e29026704d",
-		text: "Очень понравилось. Спасибо за качественную работу. Может еще раз попробовать?",
-		city: 'Санкт-Петербург',
-		platform: 'yandex'
-	},
-	{
-		id: 4,
-		name: "Екатерина",
-		lastName: "Кексова",
-		avatar: "https://i.pravatar.cc/300?u=a042581f4e29026704d",
-		text: "Очень понравилось. Спасибо за качественную работу. Может еще раз попробовать?",
-		city: 'Морки',
-		platform: '2gis'
-	},
-	{
-		id: 5,
-		name: "Ахреп",
-		lastName: "Луков",
-		avatar: "https://i.pravatar.cc/300?u=a042581f4e29026704d",
-		text: "Очень понравилось. Спасибо за качественную работу. Может еще раз попробовать?",
-		city: 'Самара',
-		platform: 'yandex'
-	},
-]
+import cls from "../Reviews/Reviews.module.scss";
 
 export const Reviews = memo(() => {
 	return (
 		<section className={classnames(styles.section, cls.reviews)}>
 			<h2 className={classnames(styles.title, cls.title)}><span>Нам</span> доверяют</h2>
 			
-			<div className={classnames(styles.reviews, cls.reviews)}>
-				{
-					reviews.map(review => <Review key={review.id} {...review}/>)
-				}
-			</div>
+			<ReviewsSlider/>
 		
 		</section>
 	)
